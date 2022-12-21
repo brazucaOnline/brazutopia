1b3062839c9f06ad45a98861bbb73b10aad93bcf
Sergio Ferreira <gobrazuca@gmail.com>
2022-12-21 18:24:49 -0500

    Fix map key

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,50 +5,56 @@ import 'react-lazy-load-image-component/src/effects/blur.css'
 import { FaHome, FaPhoneAlt, FaGlobe } from 'react-icons/fa'
 import { formatPhoneNumber } from '../../static/format'
 
-const Card = ({ item, link }) => {
+const Card = ( { item, link } ) => {
+  const streetName = item.street !== 'null' ? `${ item.street }` : ''
+  // const streetName =
+  //   item.street !== 'null' ? (
+  //     <span>{item.street}</span>, <br />
+  //   ) : (
+  //     <span></span>
+  //     )
   return (
     <li key={item.id}>
       <div>
-        <Link
-          to={`/${link}/${item.slug}/${item.city.slug}`}>
-          {
-            item.imageBanner.map(img => {
-              return (
-                <div key={item.id} className="row">
-                  <div className="title">
-                    <LazyLoadImage
-                      key={img.id}
-                      className="photo"
-                      src={img.url}
-                      effect="blur"
-                      delayMethod="debounce"
-                      alt="Banner Image"
-                    />
-                    <h3 className="city">{item.name}</h3>
-                    <h5 className="city">{item.city.name}</h5>
-                  </div>
+        <Link to={`/${link}/${item.slug}/${item.city.slug}`}>
+          {item.imageBanner.map((img) => {
+            return (
+              <div key={item.id} className='row'>
+                <div className='title'>
+                  <LazyLoadImage
+                    key={img.id}
+                    className='photo'
+                    src={img.url}
+                    effect='blur'
+                    delayMethod='debounce'
+                    alt='Banner Image'
+                  />
+                  <h3 className='city'>{item.name}</h3>
+                  <h5 className='city'>{item.city.name}</h5>
                 </div>
-              )
-            })
-          }
+              </div>
+            )
+          })}
         </Link>
-        <div className=" item address">
-          <FaHome className="icon" />
-          {item.street}, <br />
+        <div className=' item address'>
+          {streetName} 
           {item.city.name}, {item.state.stateCode} {item.postalCode}
         </div>
       </div>
-      <div className="item"><a href={`tel: +1 ${item.tel}`} className="underlined">
-        <FaPhoneAlt className="icon" /> {`${formatPhoneNumber(item.tel)}`}
-      </a></div>
-      <div className="item">
+      <div className='item'>
+        <a href={`tel: +1 ${item.tel}`} className='underlined'>
+          <FaPhoneAlt className='icon' /> {`${formatPhoneNumber(item.tel)}`}
+        </a>
+      </div>
+      <div className='item'>
         <a
           href={item.website}
-          target="_blank"
-          rel="noreferrer"
-          className="underlined">
-          <FaGlobe className="icon" />
-            Website
+          target='_blank'
+          rel='noreferrer'
+          className='underlined'
+        >
+          <FaGlobe className='icon' />
+          Website
         </a>
       </div>
     </li>
